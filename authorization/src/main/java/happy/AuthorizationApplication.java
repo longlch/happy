f68336f91c07ed124b8d63ab48226295574e7652
long.phanhoang <long.phahoang@teragroup.io>
2023-12-10 10:41:28 +0700

    [HAPPY-003] Authorization flow add manager controller

--- authorization/src/main/java/happy/AuthorizationApplication.java
+++ authorization/src/main/java/happy/AuthorizationApplication.java
@@ -1,7 +1,14 @@
 package happy;
 
+import happy.dto.RegisterRequest;
+import happy.service.AuthenticationService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static happy.constant.Role.ADMIN;
+import static happy.constant.Role.MANAGER;
 
 @SpringBootApplication
 public class AuthorizationApplication {
@@ -10,4 +17,29 @@ public class AuthorizationApplication {
         SpringApplication.run(AuthorizationApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+            var manager = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("manager@mail.com")
+                    .password("password")
+                    .role(MANAGER)
+                    .build();
+            System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+        };
+    }
 }
