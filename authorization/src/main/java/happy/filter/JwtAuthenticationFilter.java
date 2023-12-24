648026e815b6cf743a43f1895d6b9c787c15d090
long.phanhoang <long.phahoang@teragroup.io>
2023-12-24 21:15:26 +0700

    [HAPPY-004] Check refresh token

--- authorization/src/main/java/happy/filter/JwtAuthenticationFilter.java
+++ authorization/src/main/java/happy/filter/JwtAuthenticationFilter.java
@@ -51,9 +51,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmailFromToken = jwtService.extractUsername(jwt);
         if (userEmailFromToken != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmailFromToken);
-//            var isTokenValid = tokenRepository.findByToken(jwt)
-//                    .map(t -> !t.isExpired() && !t.isRevoked())
-//                    .orElse(false);
 
             if (jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
