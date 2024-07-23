7c819e65bad2a02eed6b50db691cbade06ac38e5
long.phanhoang <long.phahoang@teragroup.io>
2024-07-23 11:16:46 +0700

    add compute engine

--- learning/terraform/main.tf
+++ learning/terraform/main.tf
@@ -14,3 +14,22 @@ provider "google" { // A provider is a plugin that Terraform uses to create and 
 resource "google_compute_network" "vpc_network" { // define components of your infrastructure.
   name = "terraform-network"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  zone = "asia-east1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
