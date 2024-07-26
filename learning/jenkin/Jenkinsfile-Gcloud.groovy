b8b3e48f873f24a2c3a7cf2744593b6185876f16
long.phanhoang <long.phahoang@teragroup.io>
2024-07-26 14:35:59 +0700

    using helper

--- learning/jenkin/Jenkinsfile-Gcloud.groovy
+++ learning/jenkin/Jenkinsfile-Gcloud.groovy
@@ -2,17 +2,16 @@ pipeline {
     agent any
     environment {
         CLOUDSDK_CORE_PROJECT='happy-427410'
+        GCLOUD_CREDS=credentials('gcloud-creds')
     }
     stages {
         stage('test') {
             steps {
-                withCredentials([file(credentialsId: 'gcloud-creds', variable: 'GCLOUD_CREDS')]) {
-                    sh '''
-                        gcloud version
-                        gcloud auth activate-service-account --key-file="$GCLOUD_CREDS"
-                        gcloud compute zones list
-                    '''
-                }
+                sh '''
+          gcloud version
+          gcloud auth activate-service-account --key-file="$GCLOUD_CREDS"
+          gcloud compute zones list
+        '''
             }
         }
     }
--- learning/jenkin/LEARNING.md
+++ learning/jenkin/LEARNING.md
@@ -2,3 +2,4 @@
 https://stackoverflow.com/questions/28372328/how-to-install-the-google-cloud-sdk-in-a-docker-image
 
 # Integrate Gcloud to Jenkin
+7C7hnSnWvCDYXzMAsdpVe1EAVpyQe
