44514b62dd83184ceacf8287e85ccd054d067695
long.phanhoang <long.phahoang@teragroup.io>
2024-07-26 17:40:02 +0700

    Install sample node

--- learning/jenkin/Jenkinsfile-Gcloud.groovy
+++ learning/jenkin/Jenkinsfile-Gcloud.groovy
@@ -1,17 +1,43 @@
 pipeline {
     agent any
+
+    tools {
+        nodejs 'nodejs' // Name of the NodeJS installation
+    }
     environment {
         CLOUDSDK_CORE_PROJECT='happy-427410'
         GCLOUD_CREDS=credentials('gcloud-creds')
+        IMAGE_NAME = 'sample-node'
     }
     stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/longlch/sample-node-project.git'
+            }
+        }
+
+        stage('Install Dependencies') {
+            steps {
+                sh 'npm install'
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    docker.build("${IMAGE_NAME}:latest")
+                }
+            }
+        }
+
         stage('test') {
             steps {
                 sh '''
-          gcloud version
-          gcloud auth activate-service-account --key-file="$GCLOUD_CREDS"
-          gcloud compute zones list
-        '''
+                    echo "hello world" > hello.txt
+                    gcloud version
+                    gcloud auth activate-service-account --key-file="$GCLOUD_CREDS"
+                    gcloud compute zones list
+                '''
             }
         }
     }
