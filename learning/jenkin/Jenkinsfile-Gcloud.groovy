061564e885fef005b02c7718ed4144e3a37187b2
long.phanhoang <long.phahoang@teragroup.io>
2024-07-28 21:51:21 +0700

    push image to registry

--- learning/jenkin/Jenkinsfile-Gcloud.groovy
+++ learning/jenkin/Jenkinsfile-Gcloud.groovy
@@ -6,8 +6,11 @@ pipeline {
     }
     environment {
         CLOUDSDK_CORE_PROJECT='happy-427410'
+        PROJECT='happy-427410'
+        REPOSITORY='happy-dev'
         GCLOUD_CREDS=credentials('gcloud-creds')
-        IMAGE_NAME = 'sample-node'
+        IMAGE_NAME = 'sample-node-v2'
+        IMAGE_TAG = 'latest'
     }
     stages {
         stage('Checkout') {
@@ -22,23 +25,53 @@ pipeline {
             }
         }
 
-        stage('Build Docker Image') {
-            steps {
-                script {
-                    docker.build("${IMAGE_NAME}:latest")
-                }
-            }
-        }
-
-        stage('test') {
+        stage('Authenticate') {
             steps {
                 sh '''
                     echo "hello world" > hello.txt
                     gcloud version
                     gcloud auth activate-service-account --key-file="$GCLOUD_CREDS"
-                    gcloud compute zones list
                 '''
             }
         }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    app = docker.build("asia-east1-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}")
+                }
+            }
+        }
+
+        stage('Push to Google Cloud Registry') {
+            steps {
+                script {
+                    sh 'gcloud auth configure-docker asia-east1-docker.pkg.dev'
+                    sh 'docker push asia-east1-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}'
+
+//                    withDockerRegistry([credentialsId: 'gcloud-creds', url: "https://asia-east1-docker.pkg.dev/${PROJECT}/${REPOSITORY}"]) {
+//                        sh 'docker tag ${IMAGE_NAME}:${IMAGE_TAG} asia-east1-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}'
+//                        sh 'docker push asia-east1-docker.pkg.dev/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}'
+//                    }
+
+//                    docker.withRegistry('https://eu.gcr.io', 'gcr:gcloud-creds') {
+//                        app.push("${env.BUILD_NUMBER}")
+//                        app.push("latest")
+//                    }
+                }
+            }
+        }
     }
+
+//    post {
+//        always {
+//            cleanWs()
+//        }
+//        success {
+//            echo 'The build and push succeeded!'
+//        }
+//        failure {
+//            echo 'The build or push failed.'
+//        }
+//    }
 }
--- learning/jenkin/LEARNING.md
+++ learning/jenkin/LEARNING.md
@@ -3,3 +3,18 @@ https://stackoverflow.com/questions/28372328/how-to-install-the-google-cloud-sdk
 
 # Integrate Gcloud to Jenkin
 7C7hnSnWvCDYXzMAsdpVe1EAVpyQe
+
+def jobName = "gloud"
+def job = Jenkins.instance.getItem(jobName)
+job.getBuilds().each { it.delete() }
+job.nextBuildNumber = 1
+job.save()
+
+
+
+gcloud auth configure-docker asia-east1-docker.pkg.dev
+
+gcloud auth activate-service-account --key-file="/Users/longhoangphan/project/longlch/happy/learning/jenkin/happy-427410-8bf45eac92f5.json"
+
+docker push asia-east1-docker.pkg.dev/happy-427410/happy-dev/sample-node-v1
+gcloud auth configure-docker asia-east1-docker.pkg.dev
