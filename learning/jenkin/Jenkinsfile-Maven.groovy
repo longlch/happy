b6419ed7f06290a65767a34a46f5ebc26379367d
long.phanhoang <long.phahoang@teragroup.io>
2024-07-30 11:45:07 +0700

    can not build maven

--- learning/jenkin/Jenkinsfile-Maven.groovy
+++ learning/jenkin/Jenkinsfile-Maven.groovy
@@ -1,5 +1,17 @@
 pipeline {
     agent any
+    environment {
+        CLOUDSDK_CORE_PROJECT = 'happy-427410'
+        PROJECT = 'happy-427410'
+        REPOSITORY = 'happy-dev'
+        GCLOUD_CREDS = credentials('gcloud-creds')
+        IMAGE_NAME = 'sample-maven'
+        FINAL_IMAGE_NAME = 'sample-maven:latest'
+        IMAGE_TAG = 'latest'
+        LOCATION = 'asia-east1-docker.pkg.dev'
+        ARTIFACT_REGISTRY = "${LOCATION}/${PROJECT}/${REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}" // Corrected line
+
+    }
     stages {
         stage('Checkout') {
             steps {
@@ -11,6 +23,45 @@ pipeline {
             steps {
                 sh 'mvn clean install -DskipTest -Ddependency-check.skip=true -Dmaven.test.skip'
             }
+        }
+
+        stage('Authenticate') {
+            steps {
+                sh '''
+                    gcloud auth activate-service-account --key-file="$GCLOUD_CREDS"
+                    gcloud auth configure-docker ${LOCATION}
+                '''
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    docker.build("${FINAL_IMAGE_NAME}")
+                }
+            }
+        }
+
+        stage('Push to Google Cloud Registry') {
+            steps {
+                script {
+                    sh '''
+                        docker tag ${FINAL_IMAGE_NAME} ${ARTIFACT_REGISTRY}
+                        docker push ${ARTIFACT_REGISTRY}
+                    '''
+                }
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
+        }
+        success {
+            echo 'The build and push succeeded!'
+        }
+        failure {
+            echo 'The build or push failed.'
         }
     }
 }
